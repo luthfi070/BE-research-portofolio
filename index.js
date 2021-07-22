ceeceee553b4d968d8fab881c5582da358c4f0c9
Luthfi Fitra Musyaffa <luthfimusafa@gmail.com>
2021-07-22 21:43:22 +0700

    changed host

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use("/", (req, res) => {
   res.json({ message: "api connected" });
 });
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log("app connecsted");
 });
