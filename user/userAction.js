1b81c9dd24061f9448129e30dd99cdc711eebb86
Luthfi Fitra Musyaffa <luthfimusafa@gmail.com>
2021-08-01 21:33:04 +0700

    added jwt auth

--- user/userAction.js
+++ user/userAction.js
@@ -2,8 +2,25 @@ const express = require("express");
 const router = express.Router();
 const userModel = require("../Schema/userSchema");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-router.get("/viewUser", cors(), (req, res) => {
+router.get("/viewUser", cors(), (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (!token) {
+    return res.json({
+      auth: false,
+      msg: "no token submitted",
+    });
+  }
+
+  jwt.verify(token, "secretkey", (err, decoded) => {
+    if (err)
+      return res.json({
+        auth: false,
+        msg: "failed to auth",
+      });
+  });
+
   return userModel.findOne({ _id: req.body.id }).then((result) => {
     if (result) {
       res.json({
